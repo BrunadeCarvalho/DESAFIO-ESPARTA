7afa2b309100f012b7eb9ba57377fed2b84018f8
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-06 15:16:13 -0300

    FEAT: ProjectBusiness

--- src/business/ProjectBusiness.ts
+++ src/business/ProjectBusiness.ts
@@ -1,6 +1,7 @@
 import { ProjectDatabase } from "../data/ProjectDatabase";
 import { NotNullDescription, NotNullTitle } from "../error/ProjectError";
 import { projectInputDTO } from "../model/projects/projectInputDTO";
+import { Projects } from "../model/projects/projects";
 import { generateId } from "../services/idGenerator";
 
 export class ProjectBusiness{
@@ -16,7 +17,14 @@ export class ProjectBusiness{
 
             const generatedId: string = generateId()
 
+            const project: Projects={
+                id: generatedId,
+                title,
+                description
+            }
 
+            const projectDatabase = new ProjectDatabase
+            await projectDatabase.createProject(project)
 
         }catch(error:any){
             throw new Error(error.message)

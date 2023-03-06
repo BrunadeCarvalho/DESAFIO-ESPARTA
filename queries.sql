cd4e8fb23ef0eb19c42065b8f471643f081e55ed
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-06 14:23:46 -0300

    Query para criação das duas tabelas

---
+++ queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Projects_Esparta(
+    id VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL, 
+    description TEXT NOT NULL
+);
+
+CREATE TABLE Tasks_Esparta(
+    id VARCHAR(255) PRIMARY KEY,
+    description TEXT NOT NULL,
+    deadline DATE NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    id_projects VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_projects) REFERENCES Projects_Esparta(id)
+);
+
+

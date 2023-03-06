817425f6500448c303e4a49684871c2d62e5c918
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-06 16:07:08 -0300

    FIX: Correção no arquivo TaskController, estava com o arquivo TaskDatabase duplicado

--- src/controller/TaskController.ts
+++ src/controller/TaskController.ts
@@ -1,21 +1,25 @@
-import { BaseDatabase } from "../data/BaseDatabase"
+import { Request, Response } from "express";
+import { TasksBusiness } from "../business/TasksBusiness";
 import { Tasks } from "../model/tasks/tasks"
+import { TasksInputDTO } from "../model/tasks/tasksInputDTO";
 
-export class TasksDatabase extends BaseDatabase{
-      
-    createTasks = async(tasks: Tasks)=>{
+export class TasksController{
+    createTasks = async(req: Request, res: Response)=>{
         try{
-            await TasksDatabase.connection
-            .insert({
-                id: tasks.id,
-                description: tasks.description,
-                deadline: tasks.deadline,
-                status: tasks.status,
-                id_projects: tasks.id_projects
-            }).into("Tasks")
+            const input: TasksInputDTO={
+                description: req.body.description,
+                deadline: req.body.deadline,
+                status: req.body.status,
+                id_projects: req.body.id_projects
+            };
 
-        }catch(erro:any){
-            throw new Error(erro.message)
+            const tasksBusiness = new TasksBusiness()
+            await tasksBusiness.createTasks(input)
+
+            res.status(201).send(input)
+
+        }catch(error:any){
+            res.status(400).send(error.message || error.sqlMessage)
         }
     }
 }

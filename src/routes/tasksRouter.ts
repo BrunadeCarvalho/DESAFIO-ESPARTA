b07daa48ba575fbdeb87d1d1998b5d26111998d1
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-06 17:10:25 -0300

    FEAT: Rota para o endpoint

--- src/routes/tasksRouter.ts
+++ src/routes/tasksRouter.ts
@@ -7,3 +7,4 @@ export const tasksRouter = express.Router()
 
 tasksRouter.post("/create", tasksController.createTasks)
 tasksRouter.put("/edit/:id", tasksController.editTasks)
+tasksRouter.delete("/delete/:id", tasksController.deleteTasks)

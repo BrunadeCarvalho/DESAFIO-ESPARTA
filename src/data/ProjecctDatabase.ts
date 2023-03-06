fdc1cbbc5311ad55d14353d5971707795c9c582b
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-06 15:09:02 -0300

    FEAT: ProjectDatabase

---
+++ src/data/ProjecctDatabase.ts
@@ -0,0 +1,18 @@
+import { Projects } from "../model/projects/projects";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class ProjectDatabase extends BaseDatabase{
+    createProject = async(project: Projects)=>{
+        try{
+            await ProjectDatabase.connection
+            .insert({
+                id: project.id,
+                title: project.title,
+                description: project.description
+            }).into("Projects_Esparta")
+
+        }catch(erro:any){
+            throw new Error(erro.message)
+        }
+    }
+}
